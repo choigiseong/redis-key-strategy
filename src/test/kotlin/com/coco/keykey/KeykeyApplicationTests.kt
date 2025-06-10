d900affc0b011ebfaa4faf114f7df55e7cec5972
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2025-06-10 23:15:42 +0900

    init

---
+++ src/test/kotlin/com/coco/keykey/KeykeyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.coco.keykey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeykeyApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
